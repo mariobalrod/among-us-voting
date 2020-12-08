8c6c73d7be1e0811e5266ab39204924048647c64
Mario <marioballestero.rdg@gmail.com>
2020-12-08 18:39:07 +0100

    feat: creation players

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,75 @@ import { useEffect, useState, useCallback } from 'react';
 import { v4 } from 'uuid';
 import io from 'socket.io-client';
 
+import Form from './components/Form'; 
+
 const ENDPOINT = 'http://localhost:5000';
 const socket = io.connect(ENDPOINT);
 
 function App() {
-  const [clients, setClients] = useState([])
+  const [clients, setClients] = useState([]);
+  const [colors, setColors] = useState([
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "black",
+  ]);
+  const [hideForm, setHideForm] = useState(true);
+
+  function removeItemFromArr(arr, item) {
+    var i = arr.indexOf(item);
+    arr.splice(i, 1);
+    return arr;
+  }
 
   useEffect(() => {
-    socket.on('current_clients', (data) => {
-      setClients([...clients, data]);
+    socket.on("current_color", (data) => {
+      if (colors.includes(data)) {
+        const newColors = removeItemFromArr(colors, data);
+
+        setColors(newColors);
+      }
     });
-  }, [clients, setClients])
+
+    socket.on("current_clients", (data) => {
+      setClients([...clients, JSON.parse(data)]);
+    });
+  }, [clients, setClients, colors, setColors]);
 
   const handleLogin = useCallback(
-    () => {
-      socket.emit('clients', {name: 'Eduardo', id: socket.id})
+    (nombre, color) => {
+      if (!nombre || !color) {
+        alert("All required!");
+      } else {
+        socket.emit("color", color);
+
+        socket.emit(
+          "clients",
+          JSON.stringify({
+            username: nombre,
+            color: color,
+            votations: 0,
+            id: v4(),
+          })
+        );
+
+        setHideForm(false);
+      }
     },
-    []
-  )
+    [setHideForm]
+  );
 
   return (
     <div className="App">
-      <h1>React App</h1>
-      <button onClick={handleLogin}>Login</button> 
-      
-      {clients.map(client => (<p key={v4()}>{client.id + ' -- ' + client.name}</p>))}
+      <h1>Socketio, Flask and React Chat</h1>
+      <>
+        {hideForm && <Form colors={colors} handleLogin={handleLogin} />}
+        {!hideForm &&
+          clients.map((client) => (
+            <p key={client.id}>{JSON.stringify(client)}</p>
+          ))}
+      </>
     </div>
   );
 }
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,39 @@
+import React, { memo, useCallback, useState } from 'react';
+
+const DefaultForm = ({ colors, handleLogin}) => {
+  const [color, setColor] = useState();
+  const [nombre, setNombre] = useState();
+
+  const handleColor = useCallback((e) => {
+    e.preventDefault();
+    setColor(e.target.value);
+  }, [setColor]);
+
+  const handleName = useCallback((e) => {
+    e.preventDefault();
+    setNombre(e.target.value);
+  }, [setNombre]);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    handleLogin(nombre, color);
+  }, [handleLogin, nombre, color]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Escribe tu nombre: </label>
+      <input type="text" value={nombre} name="nombre" onChange={handleName} />
+
+      <label>Elije tu color: </label>
+      <select onChange={handleColor} value={color} name="color" >
+        <option>Default</option>
+        {colors?.map((color) => (
+          <option key={color}>{color}</option>
+        ))}
+      </select>
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
+
+export default memo(DefaultForm);
