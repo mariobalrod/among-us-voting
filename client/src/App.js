fc219453e85254e653657d09e5bc6fad6376224c
Mario <marioballestero.rdg@gmail.com>
2020-12-08 16:58:19 +0100

    feat: client connectioN

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { v4 } from 'uuid';
 import io from 'socket.io-client';
 
 const ENDPOINT = 'http://localhost:5000';
+const socket = io.connect(ENDPOINT);
 
 function App() {
+  const [clients, setClients] = useState([])
 
   useEffect(() => {
-    const socket = io.connect(ENDPOINT);
-    
-    socket.emit('message', 'hello')
-    
-    socket.on('connect', () => {
-      console.log('Connected: ', socket.id)
+    socket.on('current_clients', (data) => {
+      setClients([...clients, data]);
     });
-  }, [])
+  }, [clients, setClients])
+
+  const handleLogin = useCallback(
+    () => {
+      socket.emit('clients', {name: 'Eduardo', id: socket.id})
+    },
+    []
+  )
 
   return (
     <div className="App">
       <h1>React App</h1>
+      <button onClick={handleLogin}>Login</button> 
+      
+      {clients.map(client => (<p key={v4()}>{client.id + ' -- ' + client.name}</p>))}
     </div>
   );
 }
